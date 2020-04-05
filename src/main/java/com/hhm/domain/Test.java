395546c48500065d80c36314276cd950a39ca4fd
heduxing <46128392+heduxing@users.noreply.github.com>
2020-04-05 21:57:32 +0800

    初始化

---
+++ src/main/java/com/hhm/domain/Test.java
@@ -0,0 +1,11 @@
+package com.hhm.domain;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author HDXYA
+ * @Date 2020/4/5 21:45
+ * @Version 1.0
+ **/
+public class Test {
+}
---
+++ src/main/java/com/hhm/mapper/Te.java
@@ -0,0 +1,11 @@
+package com.hhm.mapper;
+
+/**
+ * @ClassName Te
+ * @Description TODO
+ * @Author HDXYA
+ * @Date 2020/4/5 21:49
+ * @Version 1.0
+ **/
+public class Te {
+}
